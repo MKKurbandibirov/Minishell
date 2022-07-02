5d9545648318a646020c82e488126e653be97958
Goblin Tagganaro <gtaggana@un-g3.kzn.21-school.ru>
2022-07-02 14:45:34 +0300

    help

--- source_files/execute/help_pars_here.c
+++ source_files/execute/help_pars_here.c
@@ -10,6 +10,40 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../../header_files/execute.h"
+
+static char	*ft_norm_help(char *s, int *i)
+{
+	char	*tmp;
+	int		add;
+	char	*ret;
+
+	add = 0;
+	ret = ft_strdup("");
+	*i += 1;
+	tmp = ft_ifdollar(&s[1], &add);
+	if (tmp != NULL)
+		ret = ft_strjoin_free(ret, tmp, 3);
+	else
+	{
+		free(tmp);
+		free(ret);
+		ret = NULL;
+	}
+	*i += add;
+	return (ret);
+}
+
+static char	*ft_norm_help1(char *s)
+{
+	char	*ret;
+
+	if (s[0] == '\"')
+		ret = ft_dbl_quote(s);
+	else if (s[0] == '\'')
+		ret = ft_single_quote(s);
+	return (ret);
+}
 
 char	*ft_here_dollar(char *s)
 {
@@ -18,18 +52,21 @@ char	*ft_here_dollar(char *s)
 	char	*ret;
 
 	i = 0;
+	ret = ft_strdup("");
 	while (s[i] && !ft_isspace(s[i]) && s[i] != ')' && !ft_isspec(s[i]))
 	{
-		if (s[i] == '\"')
-			quote = ft_dbl_quote(&s[i]);
-		else if (s[i] == '\'')
-			quote = ft_single_quote(&s[i]);
+		quote = NULL;
+		if (s[i] == '$')
+			quote = ft_norm_help(&s[i], &i);
 		if (s[i] == '\"' || s[i] == '\'')
 		{
+			quote = ft_norm_help1(&s[i]);
 			i += ft_skip_quote(&s[i]) + 1;
 			ret = ft_strjoin_free(ret, quote, 3);
 		}
-		else
+		else if (quote)
+			ret = ft_strjoin_free(ret, quote, 3);
+		else if (s[i] && s[i] != '$')
 			ret = ft_strjoin_c_free(ret, s[i++]);
 	}
 	return (ret);
